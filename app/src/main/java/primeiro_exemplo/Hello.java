53d730bc45836cfaa4408c24eeda6f7941936e6a
EstefaniFelix <estefaniescola1@gmail.com>
2024-02-23 12:25:50 +0000

    save

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -11,20 +11,22 @@ public class Hello {
         System.out.println(cont + " : " + nome);
         // Quando cria uma classe cria um dado, new abre um espaço na memória
         Pessoa p1 = new Pessoa();
-        p1.nome = "Estefani";
-        p1.idade = 16;
-        System.out.println(p1.nome + " : " + p1.idade);
+        // p1.nome="Marco;"
+        p1.setNome("Estefani");
+        // p1.idade= 16
+        p1.setIdade(16);
+        System.out.println(p1.getNome() + " : " + p1.getIdade());
 
-        Pessoa p2 = p1;
-        p2.nome = "Felix";
-        System.out.println(p2.nome);
-        System.out.println(p1.nome);
+        // Pessoa p2 = p1;
+        // p2.nome = "Felix";
+        // System.out.println(p2.nome);
+        // System.out.println(p1.nome);
 
-        Pessoa p3 = new Pessoa();
-        p3.nome = "Silva";
-        p3.idade = 18;
+        // Pessoa p3 = new Pessoa();
+        // p3.nome = "Silva";
+        // p3.idade = 18;
 
-        System.out.println(p1.verificarMarioridade());
-        System.out.println(p3.verificarMarioridade());
+        // System.out.println(p1.verificarMarioridade());
+        // System.out.println(p3.verificarMarioridade());
     }
 }
